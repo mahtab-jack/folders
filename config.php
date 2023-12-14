d08644e9038e32867d6365a4bd4e9d1bb522c25b
Mahtab Alam <111902189+mahtab-jack@users.noreply.github.com>
2023-12-14 18:57:08 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$db_host = "sql302.byetcluster.com";
+$db_user = "if0_35054379";
+$db_pass = "HJi8y9MXetFTIY";
+$db_name = "if0_35054379_main";
+
+$config = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("DB Not Connected");
+?>
